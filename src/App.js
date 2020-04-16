6f1932b5ede66dca7742020a199d23e92b137e91
Francesco Marassi <urcoilbisurco@gmail.com>
2020-04-16 10:04:44 +0200

    first commit for useFetchDemoApp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import useFetch from "./hooks/useFetch";
+import "./App.css";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const { response, error, loading } = useFetch(
+    "https://www.anapioficeandfire.com/api/houses",
+    {
+      query: {
+        page: currentPage,
+        pageSize: 5,
+      },
+    }
+  );
+
+  if (error) {
+    return <div className="error">{JSON.stringify(error)}</div>;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Game of Thrones Houses - Page {currentPage}</h1>
+      {loading && <div className="loading">Loading page {currentPage}</div>}
+      {!loading &&
+        response.map((data) => {
+          return (
+            <div className="datapoint" key={data.Date}>
+              <h3>{data.name}</h3>
+              {data.words && <cite>"{data.words}"</cite>}
+              {data.coatOfArms && (
+                <p>
+                  <b>Coat of Arms: </b>
+                  {data.coatOfArms}
+                </p>
+              )}
+            </div>
+          );
+        })}
+      <div className="pagination">
+        {currentPage > 1 && (
+          <button
+            onClick={() => {
+              setCurrentPage(currentPage - 1);
+            }}
+          >
+            Go to page {currentPage - 1}
+          </button>
+        )}
+        <button
+          onClick={() => {
+            setCurrentPage(currentPage + 1);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Go to page {currentPage + 1}
+        </button>
+      </div>
     </div>
   );
 }
